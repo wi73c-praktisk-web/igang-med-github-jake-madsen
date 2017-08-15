b458b666d7d0394df5ec2a9881ebf9ec0b34217a
Jake Martin Jacob Madsen <jake@iceshieldgaming.com>
2017-08-15 10:36:17 +0200

    Update to app

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hej verden")
+
+
+navn = "lars";
+var antal = 5;
+var antal2 = 10;
+
+var userInput = prompt("Indsæt dit navn", "My Name")
+
+console.log("hej", navn, "alder", antal)
+
+
+
