76ea78ba93cbcfaee1e84908d0d92d38d6816291
Jake Martin Jacob Madsen <jake@iceshieldgaming.com>
2017-08-16 10:29:04 +0200

    løkkerdel1

---
+++ løkker.js
@@ -0,0 +1,35 @@
+////////// Øvelse 1 - 3/////////////
+console.log("------ ØVELSE 1 - 3 ------");
+
+i = 0
+
+console.log("while løkke:");
+while(i <= 10)
+    {
+        console.log(i)
+        i++
+    }
+
+console.log("do while løkke:");
+do 
+    {
+        console.log(i)
+        i++;
+    }
+while (i <= 18)
+
+console.log("for løkke:");
+for(i; i <= 25 ; i++)
+    {
+        console.log(i)
+        i++;
+    }
+
+////////// Øvelse 4/////////////
+console.log("------ ØVELSE 4 ------");
+i = 2017;
+for(i; i >= 1917; i--)
+    {
+        console.log(i)
+        i--;
+    }
