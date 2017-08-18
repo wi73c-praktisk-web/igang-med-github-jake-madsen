695b869910d8c12958adf39c83a0d9bb964f7590
Jake Martin Jacob Madsen <jake@iceshieldgaming.com>
2017-08-18 13:14:37 +0200

    better fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,32 +1,32 @@
 ////////// FIZZBUZZ ////////////
+var fizzBuzz = "FizzBuzz"
+    fizz = "Fizz"
+    buzz = "Buzz"
+    i = 0;
 
-// i = 0;
-
-// for(i; i <= 100;)
-//     {
-//         if (i % 3 == 0 && i % 5 == 0)
-//             {
-//                 console.log("FizzBuzz")
-//             }
-//         else if(i % 3 == 0)
-//             {
-//                 console.log("Fizz")
-//             }
-//         else if(i % 5 == 0)
-//             {
-//                 console.log("Buzz")
-//             }
-//         else
-//             {
-//                 console.log(i)
-//             }
-//         i++;
-//     }
-
-i = 0;
+for(i; i <= 50;)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+            {
+                console.log(fizzBuzz)
+            }
+        else if(i % 3 == 0)
+            {
+                console.log(fizz)
+            }
+        else if(i % 5 == 0)
+            {
+                console.log(buzz)
+            }
+        else
+            {
+                console.log(i)
+            }
+        i++;
+    }
 
 for(i; i <= 100;)
     {
-        resultat = i % 3 == 0 && i % 5 == 0 ? console.log("FizzBuzz") : i % 3 == 0 ? console.log("Fizz") : i % 5 == 0 ? console.log("Buzz") : console.log(i);
+        resultat = i % 3 == 0 && i % 5 == 0 ? console.log(fizzBuzz) : i % 3 == 0 ? console.log(fizz) : i % 5 == 0 ? console.log(buzz) : console.log(i);
         i++;
     }
