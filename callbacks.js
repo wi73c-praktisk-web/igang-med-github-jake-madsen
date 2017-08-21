3cac79c4fd83140f5c4e6a0c6699a6dca18f4c81
Jake Martin Jacob Madsen <jake@iceshieldgaming.com>
2017-08-21 12:39:36 +0200

    update

---
+++ callbacks.js
@@ -0,0 +1,24 @@
+//////////// Øvelse 1 ///////////////
+console.log("Øvelse 1")
+
+var miniAray = ["hej", "med", "dig"];
+
+miniAray.forEach(udskrivMiniAray);
+
+function udskrivMiniAray(element,i)
+    {
+        console.log("inde i function")
+        console.log(element,i);
+    }
+
+//////////// Øvelse 2 ///////////////
+console.log("Øvelse 2")
+
+var ord = "Jeg har brug for et <p> tag"
+
+    tilføjP(ord)
+
+function tilføjP(callback, ord)
+    {
+    
+    }
