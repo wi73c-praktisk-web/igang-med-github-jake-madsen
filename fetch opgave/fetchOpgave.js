6305790af9c25bd18229e9add70be2afc42655c5
Jake Martin Jacob Madsen <jake@iceshieldgaming.com>
2017-08-22 10:54:29 +0200

    fetchdone

--- fetch opgave/fetchOpgave.js
+++ fetch opgave/fetchOpgave.js
@@ -1,9 +1,49 @@
-fetch('https://swapi.co/api/people/')
-.then((response) => {
-   // grib svarets indhold (body) og send det som et json objekt til næste .then()
-   return response.json();
-})
-.then((data) => {
-   // nu er json objektet lagt ind i data variablen, udskriv data
-   console.log(data);
-})
+function getStarWarsApi(isClicked)
+{
+    var starwarsPersonArray = [];
+        mit_id = document.getElementById("arrayId");
+        mit_id.innerHTML = "";
+        forNavn = "";
+        kon = "";
+        fodselsAr = "";
+
+
+
+    fetch('https://swapi.co/api/people/')
+        .then((response) => 
+            {   
+                if(!response.ok) // Hvis response ikke er gået igennem, stop funktion og smid fejl til catch
+                    {
+                        throw Error(response.statusText);
+                    }
+                return response.json();
+            })
+        .then((data) => 
+            {
+                starwarsPersonArray = data.results
+                // console.log(starwarsPersonArray)
+
+                starwarsPersonArray.forEach(function(element, index)
+                    {
+                        forNavn = starwarsPersonArray[index].name;
+                        kon = starwarsPersonArray[index].gender;
+                        fodselsAr = starwarsPersonArray[index].birth_year;
+
+                        mit_id.innerHTML += "<li>" + "Fornavn: " + forNavn + " Køn: " +  kon + " Fødsels År: " + fodselsAr + "</li>";
+                        // console.log("Fornavn:", forNavn, "Køn:", kon, "Fødsels År:", fodselsAr,)
+                    }, 
+                this);
+            })
+        .catch((error) =>   
+            {
+                mit_id.innerHTML = "Der skete en fejl: " + error;
+                console.log(error)
+            });
+    
+        
+    
+    document.getElementById("hent_knap").style.visibility = "hidden";
+    isClicked = true;
+
+        
+}
